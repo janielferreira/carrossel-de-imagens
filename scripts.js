d65fb2f9c2c01c7c07b35b3bf50638ae7df31a1a
Janiel Ferreira <janielferreira1999@gmail.com>
2026-09-19 16:47:59 -0300

    Terminado Javascript e começando as animações no CSS
    Irei começar as animações pelo CSS, estou querendo criar alguns keyframes e animar para deixar a transição das imagens mais suaves.

--- scripts.js
+++ scripts.js
@@ -13,7 +13,19 @@ function MovimentoAoClicar(type) {
     let listItems = document.querySelectorAll(".list .list-item")
     let thumbItems = document.querySelectorAll(".thumb .thumb-item")
 
-    console.log(listItems)
-    console.log(thumbItems)
+    if(type === "next") {
+        list.appendChild(listItems[0])
+        thumb.appendChild(thumbItems[0])
+        container.classList.add("animation-next")
+    } else {
+        list.prepend(listItems[3])
+        thumb.prepend(thumbItems[3])
+        container.classList.add("animation-back")
+    }
+
+    setTimeout(() => {
+        container.classList.remove("animation-next")
+        container.classList.remove("animation-back")
+    }, 3000);
 }
 
